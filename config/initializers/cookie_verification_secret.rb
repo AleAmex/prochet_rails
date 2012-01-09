49dc3b79309314fa6c2b4a93d7acd2e817b39e8b
aleamex <aleamex@aleamex-TravelMate-800.(none)>
2012-01-10 00:27:42 +0100

    inizialize project

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '9fa99c3a5853d495336b0328abcbd2d1a20f9e044f88a8f1152a334457f3dd3631afed8ef628a79921e2805ffc161e7cbca8df022c69c1c87e2bc9bee4794539';
